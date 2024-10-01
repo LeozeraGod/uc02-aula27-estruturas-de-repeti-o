59b164e4c02613f47b8aa74600253c8e511260ec
Leonardo Souza <leonardomellolopes@gmail.com>
2024-10-01 19:23:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//While 
+//do/while 
+//for 
+
+
+//Crie uma lista com 5 nomes usando um while 
+
+//enquanto {condição verdadeira} faça lguma coisa 
+
+let numeroDeVezes = 0
+
+while(numeroDeVezes < 5) {
+    console.log("PASSOU AQUI")
+        numeroDeVezes = numeroDeVezes + 1
+}
