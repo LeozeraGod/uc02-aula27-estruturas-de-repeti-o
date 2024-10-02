54a4352c07ef9d6d99d4f28c5499d33658269169
Leonardo Souza <leonardomellolopes@gmail.com>
2024-10-01 21:35:51 -0300

    Second Commit

--- script.js
+++ script.js
@@ -5,11 +5,68 @@
 
 //Crie uma lista com 5 nomes usando um while 
 
-//enquanto {condição verdadeira} faça lguma coisa 
+//enquanto {condição verdadeira} faça alguma coisa 
 
-let numeroDeVezes = 0
+//lista de array ou vetor
+let lista = ['Banana', 'Maça', 'Kiwi', 'Morangos', 'Laranjas']
 
-while(numeroDeVezes < 5) {
-    console.log("PASSOU AQUI")
-        numeroDeVezes = numeroDeVezes + 1
+//cria uma variavel de Contador 
+let posicao = 0
+
+//Cria uma lista não ordenada
+const listaEmTela = document.createElement("ol")
+
+while (posicao < 5) {
+    console.log(lista[posicao])
+    const itemLista = document.createElement("li")
+    itemLista.textContent = lista[posicao]
+    listaEmTela.appendChild(itemLista)
+    posicao = posicao + 1
+}
+
+
+document.body.appendChild(listaEmTela)
+
+
+
+let sexo 
+
+do {
+    sexo = prompt("Informe seu Sexo (M-Masculino; F-Feminino): ")
+} while (sexo) 
+
+    console.log(sexo)
+/* 
+Comparações Truthy
+variavel != undefined 
+variavel != null
+variavel > 0
+variavel = True 
+
+Comprarações falsy
+variavel == undefined
+variavel == null 
+variavel == 0
+variavel == false 
+variavel == " "
+    */
+
+
+
+//! For - para 
+//! Usos do for quando eu sei o tamanho da minha lista ou quantidade 
+//! que vou executar a repetição
+//! Para uma variável index que começa em 0
+//! condição (enquanto o index < tamanho da lista)
+//! Incremente em 1 a variável index 
+
+for (let index = 0; index < lista.length; index++) {
+    const element = lista [index]
+    console.log(element)
+}
+
+//? FOR Each
+
+for (let item of lista) {
+    console.log(item)
 }
---
+++ script01.js
@@ -0,0 +1,34 @@
+//! Mostrar o nome do mês com base no número do mês
+
+const nomeMeses = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril", 
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+]
+
+const mes = prompt("Informe o mês")
+const numeroMes = Number(mes)
+
+let mensagem = "Mês Inválido"
+
+if((numeroMes != NaN) && (numeroMes > 0) && (numeroMes <=12)) {
+    mensagem = `${nomeMeses[numeroMes - 1]} é o mês de número ${numeroMes}`
+}else {
+    for (let index = 0; index < nomeMeses.length; index++) {
+        if (mes.toLowerCase() === nomeMeses[index].toLowerCase()){
+            mensagem = `${mes} é o mês de número ${index + 1}`
+            break
+        }
+    }
+}
+
+console.log(mensagem)
